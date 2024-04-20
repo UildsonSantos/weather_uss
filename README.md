23c4bac6e52df877c34686da1458f40600b69fd3
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-04-20 03:50:47 -0300

    initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather_uss
+
+A new Flutter project.
---
+++ android/app/src/main/java/dos/santos/uildson/weather_uss/MainActivity.java
@@ -0,0 +1,6 @@
+package dos.santos.uildson.weather_uss;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
