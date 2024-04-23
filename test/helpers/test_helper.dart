d23ab17c3eae80a9217553800e84b041efe57cb6
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-04-23 01:56:12 -0300

    create test for weatherBloc

--- test/helpers/test_helper.dart
+++ test/helpers/test_helper.dart
@@ -1,12 +1,14 @@
 import 'package:mockito/annotations.dart';
+import 'package:http/http.dart' as http;
 import 'package:weather_uss/data/data_source/remote_data_source.dart';
 import 'package:weather_uss/domain/repositories/weather_repository.dart';
-import 'package:http/http.dart' as http;
+import 'package:weather_uss/domain/usecases/get_current_weather.dart';
 
 @GenerateMocks(
   [
     WeatherRepository,
     WeatherRemoteDataSource,
+    GetCurrentWeatherUseCase,
   ],
   customMocks: [MockSpec<http.Client>(as: #MockHttpClient)],
 )
